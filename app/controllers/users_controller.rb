72b10a376ba8bb2db170b8e9e46c51e7fb4fb714
keroken <kenichi.shinagawa@gmail.com>
2015-07-12 16:48:59 +0000

    add edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -18,9 +19,24 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to root_path , notice: 'プロフィールを更新しました'
+    else
+      render 'edit'
+    end
+  end
+  
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :location, :age)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post   'login'  => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   
+  
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 end
