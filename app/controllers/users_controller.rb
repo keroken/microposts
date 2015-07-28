83dfd6a5053abb0ebe32ce86bfe9ed69bb081a13
keroken <kenichi.shinagawa@gmail.com>
2015-07-28 12:16:55 +0000

    add user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,19 @@ class UsersController < ApplicationController
     end
   end
   
+  def following
+    @user = User.find(params[:id])
+    @users = @user.followed_users
+    render 'show_following'
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.following_users
+    render 'show_followed'
+  end
+  
+  
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
